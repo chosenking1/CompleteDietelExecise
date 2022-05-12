2337e0ce4e6f33495e8a25e625c4c220d6387410
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2022-05-12 15:51:22 +0100

    Update

---
+++ src/chapterTwo/CheckerboardPatternOfAsterisks.java
@@ -0,0 +1,17 @@
+package chapterTwo;
+//2.27
+public class CheckerboardPatternOfAsterisks {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * * *");
+
+        System.out.println(" * * * * * * * *");
+
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+
+    }
+}
---
+++ src/chapterTwo/DiameterCircumferenceAndAreaOfACircle.java
@@ -0,0 +1,8 @@
+package chapterTwo;
+
+public class DiameterCircumferenceAndAreaOfACircle {
+
+    public static String calculate(int radius) {
+        return (2*radius + " " + 2 * Math.PI * radius + " " + Math.PI * radius * radius);
+    }
+}
